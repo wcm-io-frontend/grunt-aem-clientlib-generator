c30a26560033333292d08a7520883276be8709f0
Matthias Hauch <mhauch@pro-vision.de>
2017-02-28 14:36:12 +0100

    fix missing jsProcessor property

--- tasks/clientlib.js
+++ tasks/clientlib.js
@@ -29,7 +29,7 @@ module.exports = function(grunt) {
     var clientLibKeys = ["embed", "dependencies", "cssProcessor", "jsProcessor"];
     var assetDep = {
       "cssProcessor": "css",
-      "jsProcessor": "css"
+      "jsProcessor": "js"
     };
     var clientLibName = this.target;
     var options       = this.options({
